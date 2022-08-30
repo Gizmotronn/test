31f8c6d8393b5b12ca9e109fd6192237ad5347d4
Andrew Henry <andrewmhenry@me.com>
2022-08-30 12:08:42 +1000

    ➕ feat(ForgeButton): refactored inline CSS to use 'styled' Emotion

--- components/ForgeButton.js
+++ components/ForgeButton.js
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import styled from '@emotion/styled'
 import ForgeGlobe from './ForgeGlobe'
 
 const ForgeButton = (props) => {
-  const [isPlaying, setIsPlaying] = useState(false)
   const router = useRouter()
 
   const { forge, size } = props
 
   return (
     <>
-      <button
-        onClick={() => router.push('/forge')}
-        type='button'
-        style={{ background: 'inherit', padding: 0, border: 'none', cursor: forge ? 'pointer' : 'not-allowed' }}
-      >
+      <Button onClick={() => router.push('/forge')} type='button' forge={forge}>
         <ForgeGlobe forge={forge} size={size} />
-      </button>
+      </Button>
     </>
   )
 }
 
 export default ForgeButton
+
+const Button = styled.button`
+  background: inherit;
+  padding: 0;
+  border: none;
+  cursor: ${(props) => (props.forge ? 'pointer' : 'not-allowed')};
+`
