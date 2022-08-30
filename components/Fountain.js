96b4015e0c9498ec90025283c8e2ae329bf123cc
Andrew Henry <andrewmhenry@me.com>
2022-08-30 22:13:55 +1000

    ➕ feat(Forge page): implemented Fountain view with nft availability counter

--- components/Fountain.js
+++ components/Fountain.js
@@ -4,22 +4,7 @@ import FountainAnimation from './FountainAnimation'
 
 const Fountain = (props) => {
   const { fountainSize } = props
-  return (
-    // <Container minWidthContainer={minWidthContainer} minHeightContainer={minHeightContainer}>
-    <FountainAnimation fountainSize={fountainSize} />
-    // </Container>
-  )
+  return <FountainAnimation fountainSize={fountainSize} />
 }
 
 export default Fountain
-
-// const Container = styled.div`
-//   min-width: ${(props) => props.minWidthContainer};
-//   min-height: ${(props) => props.minHeightContainer};
-//   background: inherit;
-//   border: 1px solid red;
-//   margin: 10px 10px 10px 5px;
-//   @media (max-width: 1023px) {
-//     margin: 5px 10px 10px 10px;
-//   }
-// `
--- pages/forge.js
+++ pages/forge.js
@@ -1,81 +1,58 @@
 import styled from '@emotion/styled'
-import Chamber from '../components/Chamber'
 import Fountain from '../components/Fountain'
-import AudioButton from '../components/AudioButton'
 
 export default function Forge() {
   // 👇 min size @ 1024px
   const fountainSize = '486px' // 486px
-  const chamberSize = '328px' // 328px
-  const globeSize = '182px' // 182px
 
-  const nftData = [
-    {
-      nft: 1,
-      name: 'BricktoPunks',
-      mint: true,
-    },
-    {
-      nft: 2,
-      name: 'Beers',
-      mint: false,
-    },
-    {
-      nft: 3,
-      name: 'Super Computer',
-      mint: false,
-    },
-    {
-      nft: 4,
-      name: 'The Forge',
-      mint: false,
-    },
-    {
-      nft: 5,
-      name: 'Brain',
-      mint: false,
-    },
-  ]
+  const nftAvailability = '750/999'
 
   return (
     <LandingContainer>
-      <ChamberContainer>
-        <Chamber chamberSize={chamberSize} globeSize={globeSize} nftData={nftData} />
-      </ChamberContainer>
       <FountainContainer>
-        {/* <div style={{ position: 'absolute', right: '15px' }}>
-          <li style={{ listStyle: 'none', display: 'contents' }}>
-            <AudioButton />
-          </li>
-        </div> */}
-        <Fountain fountainSize={fountainSize} />
+        <RelativeContainer>
+          <AvailabilityContainer>
+            <Availability>{nftAvailability}</Availability>
+          </AvailabilityContainer>
+          <Fountain fountainSize={fountainSize} />
+        </RelativeContainer>
       </FountainContainer>
     </LandingContainer>
   )
 }
 
 const LandingContainer = styled.div`
-  ${'' /* border: 1px solid yellow; */}
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  width: 100%;
   margin: 0 auto;
   @media (max-width: 1023px) {
     display: none;
   }
 `
 
-const ChamberContainer = styled.div`
-  ${'' /* border: 1px solid blue; */}
-  width: 50%;
+const FountainContainer = styled.div`
+  display: 'flex';
+  justify-content: flex-end;
+  align-items: center;
   background: inherit;
 `
 
-const FountainContainer = styled.div`
-  ${'' /* border: 1px solid red; */}
-  width: 50%;
+const RelativeContainer = styled.div`
+  position: relative;
+`
+
+const AvailabilityContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  right: -55%;
+  transform: translate(-50%, -50%);
   background: inherit;
-  ${'' /* margin: 0 0 0 15px; */}
+`
+
+const Availability = styled.p`
+  color: #ffffff;
+  font-size: 32px;
+  text-align: left;
 `
