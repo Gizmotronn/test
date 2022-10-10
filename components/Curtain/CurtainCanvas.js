9adba014c88800fcf62c8c44fb92dc704a7a8afa
Andrew Henry <andrewmhenry@me.com>
2022-10-10 12:44:09 +0800

    ➕ feat(Curtain and App): testing our new Curtain assets and removing some unused code

--- components/Curtain/CurtainCanvas.js
+++ components/Curtain/CurtainCanvas.js
@@ -6,35 +6,41 @@ export default function CurtainCanvas(props) {
   const [images, setImages] = useState([])
 
   const [index, setIndex] = useState(0)
+  console.log('🚀 ~ file: CurtainCanvas.js ~ line 9 ~ CurtainCanvas ~ index', index)
 
   const { route } = props
 
-  const frameCount = 240
+  const frameCount = 49
   const canvasWidth = 1600
   const canvasHeight = 1000
 
   // 👇 Get the current frame image
-  const getCurrentFrame = (index) =>
-    `https://res.cloudinary.com/bricktorigins/image/upload/q_auto,f_auto/v1664857485/curtain/Please_Be_Seated.0001${index
+  const getCurrentFrame = (frameIndex) =>
+    `https://res.cloudinary.com/bricktorigins/image/upload/q_auto,f_auto/v1664857485/curtain/49/Please_Be_Seated.${frameIndex
       .toString()
       .padStart(3, '0')}.png`
 
+  //   const getCurrentFrame = (index) =>
+  // `https://res.cloudinary.com/bricktorigins/image/upload/q_auto,f_auto/v1664857485/curtain/Please_Be_Seated.0001${index
+  //   .toString()
+  //   .padStart(3, '0')}.png`
+
   // 👇 Handle scroll / up arrow BACK
   const prevIndex = () => {
-    if (index === 0) {
+    if (index === 1) {
       return
     }
 
-    return setIndex(index - 5)
+    return setIndex(index - 1)
   }
 
   // 👇 Handle scroll / down arrow FORWARD
   const nextIndex = () => {
-    if (index === frameCount - 5) {
+    if (index === frameCount - 1) {
       return
     }
 
-    return setIndex(index + 5)
+    return setIndex(index + 1)
   }
 
   // 👇 Render the CANVAS
@@ -47,7 +53,7 @@ export default function CurtainCanvas(props) {
   useEffect(() => {
     // 👇 Loop over the frames and add them to the images array as state
     function preloadImages() {
-      for (let i = 0; i <= frameCount - 1; i++) {
+      for (let i = 1; i <= frameCount; i++) {
         const img = new Image()
         const imgSrc = getCurrentFrame(i)
         img.src = imgSrc
@@ -94,7 +100,7 @@ export default function CurtainCanvas(props) {
                 transform: 'translate(-50%, -50%)',
                 width: '100vw',
                 height: '100vh',
-                zIndex: index === 235 ? '0' : '500',
+                zIndex: index === 49 ? '0' : '500',
               }}
             />
           </ReactScrollWheelHandler>
