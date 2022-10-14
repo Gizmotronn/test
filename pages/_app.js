efc43e7da5eca89f31a1bdd16f9d3518bcedc594
Andrew Henry <andrewmhenry@me.com>
2022-10-14 13:15:04 +0800

    ➕ feat(Moralis Auth): created a Request Message Endpoint and configured NextAuth

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { publicProvider } from 'wagmi/providers/public'
 // import { infuraProvider } from 'wagmi/providers/infura'
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { MoralisProvider } from 'react-moralis'
+import { SessionProvider } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import useWindowSize from '../hooks/useWindowSize'
 import AudioProvider from '../contexts/AudioContext'
@@ -14,7 +15,7 @@ import '../styles/fonts.css'
 
 // const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
 
-const { chains, provider } = configureChains(
+const { chains, provider, webSocketProvider } = configureChains(
   [chain.polygon],
   [publicProvider()]
   // [infuraProvider({ infuraId }), publicProvider()]
@@ -29,6 +30,7 @@ const wagmiClient = createClient({
   autoConnect: true,
   connectors,
   provider,
+  webSocketProvider,
 })
 
 function MyApp({ Component, pageProps }) {
@@ -39,16 +41,18 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <AudioProvider>
-          <MoralisProvider appId={process.env.NEXT_PUBLIC_APP_ID} serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}>
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <RainbowKitProvider chains={chains}>
+          <AudioProvider>
+            {/* <MoralisProvider appId={process.env.NEXT_PUBLIC_APP_ID} serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}> */}
             <IntroModal showModal={true} route={appRoute} />
             <Layout windowSize={size}>
               <Component {...pageProps} windowSize={size} />
             </Layout>
-          </MoralisProvider>
-        </AudioProvider>
-      </RainbowKitProvider>
+            {/* </MoralisProvider> */}
+          </AudioProvider>
+        </RainbowKitProvider>
+      </SessionProvider>
     </WagmiConfig>
   )
 }
